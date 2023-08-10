e39e3720efbbcaeb426d57d82540086419ae9ccc
Serhiy8 <markovsm86@gmail.com>
2023-08-10 22:12:04 +0300

    Update colorHex.jsx

--- src/components/colorHex.jsx
+++ src/components/colorHex.jsx
@@ -1,10 +1,10 @@
 export const getRandomHexColor = () => {
-  let red = Math.floor(Math.random() * 256);
-  let green = Math.floor(Math.random() * 256);
-  let blue = Math.floor(Math.random() * 256);
+  let red = Math.floor(Math.random() * 256).toString(16);
+  let green = Math.floor(Math.random() * 256).toString(16);
+  let blue = Math.floor(Math.random() * 256).toString(16);
 
   if(red > 30 || green > 30 || blue > 30) {
-    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;       
+    return `#${red.padStart(2, '0')}${green.padStart(2, '0')}${blue.padStart(2, '0')}`;       
   }
 
    return getRandomHexColor();
